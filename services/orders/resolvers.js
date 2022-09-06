e075d0b37a6c9672a25e8efa0ac7d937fccfdb2b
kschrade <kyleschrade2@gmail.com>
2022-09-06 08:33:27 -0400

    orders service

--- services/orders/resolvers.js
+++ services/orders/resolvers.js
@@ -1,18 +1,9 @@
-import { authors } from "./data.js";
+import { orders } from './data.js';
 
 export const resolvers = {
-  Author: {
-    __resolveReference(reference, _, __) {
-      return authors.find(author => author.id === parseInt(reference.id));
-    }
-  },
-
   Query: {
-    author(_, { id }, __, ___) {
-      return authors.find(author => author.id === parseInt(id));
+    order(_, { id }, __, ___) {
+      return orders.find((order) => order.id === id);
     },
-    authors() {
-      return authors;
-    }
-  }
+  },
 };
