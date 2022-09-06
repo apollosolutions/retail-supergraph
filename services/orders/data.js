a9ff30312bf0bf1168cae2c8b98d485e1f229ab2
Kyle Schrade <kyleschrade2@gmail.com>
2022-09-06 09:06:59 -0400

    Orders data (#12)

--- services/orders/data.js
+++ services/orders/data.js
@@ -1,4 +1,8 @@
-export const authors = [
-  {id: 1, name: "Alice"},
-  {id: 2, name: "Bob"}
+export const orders = [
+  { id: "1", buyer: { id: 1 }, items: [{ id: 2 }, { id: 2 }] },
+  { id: "2", buyer: { id: 1 }, items: [{ id: 1 }] },
+  { id: "3", buyer: { id: 2 }, items: [{ id: 3 }, { id: 1 }] },
+  { id: "4", buyer: { id: 3 }, items: [{ id: 1 }] },
+  { id: "5", buyer: { id: 3 }, items: [{ id: 2 }, { id: 1 }] },
+  { id: "6", buyer: { id: 3 }, items: [{ id: 3 }, { id: 4 }, { id: 1 }] },
 ];
