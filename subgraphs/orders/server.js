fdda4063747dadc16bbd32a48c05e6cc8a3b70f5
Lenny Burdette <lenny@apollographql.com>
2022-10-06 10:46:22 -0700

    upgrade to AS 4

--- subgraphs/orders/server.js
+++ subgraphs/orders/server.js
@@ -1,23 +1,30 @@
-import {ApolloServer, gql} from "apollo-server";
-import {ApolloServerPluginLandingPageLocalDefault} from "apollo-server-core";
-import {buildSubgraphSchema} from "@apollo/subgraph";
-import {resolvers} from "./resolvers.js";
-import {readFileSync} from "fs";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { parse } from "graphql";
+import { buildSubgraphSchema } from "@apollo/subgraph";
+import { resolvers } from "./resolvers.js";
+import { readFileSync } from "fs";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const typeDefs = gql(readFileSync(resolve(__dirname, "schema.graphql"), "utf8"));
-const schema = buildSubgraphSchema([{typeDefs, resolvers}]);
+const typeDefs = parse(
+  readFileSync(resolve(__dirname, "schema.graphql"), "utf8")
+);
+const schema = buildSubgraphSchema([{ typeDefs, resolvers }]);
 const server = new ApolloServer({
   schema,
   cache: "bounded",
   csrfPrevention: true,
-  plugins: [ApolloServerPluginLandingPageLocalDefault({embed: true})]
 });
 
 export const start = async (port) => {
   const serverPort = port ?? process.env.PORT;
-  const { url } = await server.listen(serverPort);
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: serverPort },
+    async context({ req }) {
+      return { headers: req.headers };
+    },
+  });
   console.log(`📝 Orders subgraph running at ${url}`);
 };
