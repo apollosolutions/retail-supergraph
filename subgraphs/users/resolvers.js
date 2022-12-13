245eda2a2bd789ba818ca1cfa103a6cc7dd677b5
Shane Myrick <shane@apollographql.com>
2022-12-13 11:12:38 -0800

    Add user error message

--- subgraphs/users/resolvers.js
+++ subgraphs/users/resolvers.js
@@ -1,4 +1,5 @@
 import { users } from "./data.js";
+import { GraphQLError } from "graphql";
 
 const getUserById = (id) => users.find((it) => it.id === id);
 
@@ -6,12 +7,13 @@ export const resolvers = {
   Query: {
     user(_, __, context) {
       const userId = context.headers["x-user-id"];
+      const user = getUserById(userId);
 
-      if (!userId) {
-        return null;
+      if (!user) {
+        throw new GraphQLError("Could not locate user by id. Please specify a valid `x-user-id` header like `user:1`");
       }
 
-      return getUserById(userId);
+      return user;
     },
   },
   User: {
