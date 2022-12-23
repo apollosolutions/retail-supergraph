1c07128d1463e2d87b38bce1ddeb34c898df6c65
Mandi Wise <hello@mandiwise.com>
2022-12-23 13:00:58 -0700

    Switch to UPC for product key in reviews subgraph. (#75)

--- subgraphs/reviews/resolvers.js
+++ subgraphs/reviews/resolvers.js
@@ -1,13 +1,13 @@
 import { REVIEWS } from "./data.js";
 
 export const getReviewsById = (reviewId) => REVIEWS.find((it) => it.id === reviewId);
-export const getReviewsByProductId = (productId) => REVIEWS.filter((it) => it.product.id === productId);
+export const getReviewsByProductUpc = (productUpc) => REVIEWS.filter((it) => it.product.upc === productUpc);
 
 export const resolvers = {
   Review: {
     __resolveReference: (ref) => getReviewsById(ref.id)
   },
   Product: {
-    reviews: (parent) => getReviewsByProductId(parent.id)
+    reviews: (parent) => getReviewsByProductUpc(parent.upc)
   }
 };
